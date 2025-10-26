a80fbb752488ae3236713f45999dd796011f2c61
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-26 19:18:31 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir, stat } from "node:fs/promises";
+import path from "path";
+
 const list = async () => {
-  // Write your code here
+  try {
+    const dirPath = path.join("src/fs/files");
+    const files = await readdir(dirPath);
+    for (const file of files) {
+      const filePath = path.join(dirPath, file);
+      const fileInfo = await stat(filePath);
+      if (fileInfo.isFile()) {
+        console.log(file);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    throw Error("FS operation failed");
+  }
 };
 
 await list();
