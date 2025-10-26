c149e2807e6ad184f0daca4abea4515a0d809942
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-27 00:01:32 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-  // Write your code here
+  const transformStream = new Transform({
+    transform(chunk, _, callback) {
+      const res = chunk.toString().split("").reverse().join("");
+      callback(null, `${res}\n`);
+    }
+  })
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
