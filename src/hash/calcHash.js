7197fc135e4193f8e54455045b9548ea849ce21d
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-26 22:44:43 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import path from "path";
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = path.join("src/hash/files/fileToCalculateHashFor.txt");
+  const stream = createReadStream(filePath);
+  const hash = createHash("sha256");
+  for await (const chunk of stream) {
+    hash.update(chunk);
+  }
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();
