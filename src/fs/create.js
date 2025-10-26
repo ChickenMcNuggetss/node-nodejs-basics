820ac823adb54b860bdb0a2a9d3e24196fffd951
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-26 14:38:31 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-  // Write your code here
+  try {
+    await writeFile('src/fs/files/fresh.txt', 'I am fresh and young', {flag: 'wx'});
+  } catch {
+    throw Error('FS operation failed');
+  }
 };
 
 await create();
