08834fc562a0ec40c9ce7160f81a917bf102efd6
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-26 20:14:04 +0300

    feat: add parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+  const result = Object.entries(process.env)
+    .filter((env) => env[0].includes("RSS_"))
+    .map((env) => {
+      return `${env[0]}=${env[1]}`;
+    })
+    .join("; ");
+  console.log(result);
 };
 
 parseEnv();
