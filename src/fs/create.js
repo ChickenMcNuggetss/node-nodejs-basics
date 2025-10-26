201354600ec97eee0d35e2af42debc5ef0eac17c
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-10-26 18:08:47 +0300

    feat: add rename function

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,7 +3,8 @@ import { writeFile } from 'node:fs/promises';
 const create = async () => {
   try {
     await writeFile('src/fs/files/fresh.txt', 'I am fresh and young', {flag: 'wx'});
-  } catch {
+  } catch (err) {
+    console.log(err);
     throw Error('FS operation failed');
   }
 };
